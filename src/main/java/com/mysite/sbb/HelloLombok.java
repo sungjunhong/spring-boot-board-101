75bfa5cb103e5673fc1fccd08be175b4d0249e72
Sungjun Hong <sjhong@sculpin.local>
2024-01-08 21:24:36 +0900

    Use @RequiredArgsConstructor

--- src/main/java/com/mysite/sbb/HelloLombok.java
+++ src/main/java/com/mysite/sbb/HelloLombok.java
@@ -1,20 +1,17 @@
 package com.mysite.sbb;
 
 import lombok.Getter;
-import lombok.Setter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
-@Setter
+@RequiredArgsConstructor
 public class HelloLombok {
 
-    private String hello;
-    private int lombok;
+    private final String hello;
+    private final int lombok;
 
     public static void main(String[] args) {
-        HelloLombok helloLombok = new HelloLombok();
-        helloLombok.setHello("헬로");
-        helloLombok.setLombok(5);
-
+        HelloLombok helloLombok = new HelloLombok("Hello", 5);
         System.out.println(helloLombok.getHello());
         System.out.println(helloLombok.getLombok());
     }
